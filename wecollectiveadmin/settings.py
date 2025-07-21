28b4d54e01facee088230d581c158309bbc32b05
karouhifar <karouhifar@gmail.com>
2025-07-21 14:54:31 -0400

    updated template for the input file

--- wecollectiveadmin/settings.py
+++ wecollectiveadmin/settings.py
@@ -16,7 +16,7 @@ from urllib.parse import urlparse
 load_dotenv()
 url = urlparse(os.getenv("DATABASE_URL"))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
